186eb54b0c386d249e9396553d98bec614868230
Eric Larson <ewlarson@gmail.com>
2013-01-23 12:36:28 -0600

    Use kmeans clustering to determine dominate colors, convert RGB to LAB for better human color perception colorspace

--- colors.rb
+++ colors.rb
@@ -1,9 +1,18 @@
 #!/Users/ewlarson/.rvm/rubies/ruby-1.9.3-p327/bin/ruby
 
+# @TODO
+# 1) Extract color conversion
+# 2) Solr connection doesn't belong here
+# 3) Miro options should be configurable
+# 4) Key colors should be configurable
+# 5) All metadata should be isolated
+
+
 require 'rubygems'
 require 'mechanize'   # Crawl UW Digital Collection for data, images
 require 'chunky_png'  # Pure Ruby image manipulation
-require 'miro'        # Detect dominant image colors
+require 'miro'        # Detect dominant image colors 
+require 'ai4r'        # Kmeans
 require 'rsolr'       # Solr
 require 'fileutils'   # Save files
 require 'awesome_print'
@@ -26,7 +35,7 @@ end
 # Miro options
 Miro.options[:image_magick_path] = '/usr/local/bin/convert'
 Miro.options[:resolution] = '100x100'
-Miro.options[:color_count] = 4
+Miro.options[:color_count] = 1000
 
 # Configure for Solr
 # - Using the default apache-solr-4.0.0/example/solr/collection1/conf/ schema.xml solrconfig.xml
@@ -50,7 +59,85 @@ def key_colors
   #[51,51,51]      =>  "Tungsten",
   #[204,204,204]   =>  "Silver",
 
-  { [128,0,0] => 'Cayenne', [128,128,0] => 'Asparagus', [0,128,0] => 'Clover', [0,128,128] => 'Teal', [0,0,128] => 'Midnight', [128,0,128] => 'Plum', [128,64,0] => 'Mocha', [64,128,0] => 'Fern', [0,128,64] => 'Moss', [0,64,128] => 'Ocean', [64,0,128] => 'Eggplant', [128,0,64] => 'Maroon', [255,0,0] => 'Maraschino', [255,255,0] => 'Lemon', [0,255,0] => 'Spring', [0,255,255] => 'Turquoise', [0,0,255] => 'Blueberry', [255,0,255] => 'Magenta', [255,128,0] => 'Tangerine', [128,255,0] => 'Lime', [0,255,128] => 'Sea Foam', [0,128,255] => 'Aqua', [128,0,255] => 'Grape', [255,0,128] => 'Strawberry', [255,102,102] => 'Salmon', [255,255,102] => 'Banana', [102,255,102] => 'Flora', [102,255,255] => 'Ice', [102,102,255] => 'Orchid', [255,102,255] => 'Bubblegum', [25,25,25] => 'Lead', [230,230,230] => 'Mercury', [255,204,102] => 'Cantaloupe', [204,255,102] => 'Honeydew', [102,255,204] => 'Spindrift', [102,204,255] => 'Sky', [204,102,255] => 'Lavender', [255,111,207] => "Carnation", [0,0,0] => "Licorice", [255,255,255] => 'Snow' }
+  { [25.531, 48.055, 38.060] => 'Cayenne', [51.868, -12.931, 56.677] => 'Asparagus', [46.229, -51.700, 49.898] => 'Clover', [48.256, -28.842, -8.481] => 'Teal', [12.975, 47.508, -64.704] => 'Midnight', [29.782, 58.940, -36.498] => 'Plum', [34.510, 23.976, 44.820] => 'Mocha', [47.660, -39.672, 51.639] => 'Fern', [46.722, -45.498, 26.354] => 'Moss', [27.367, 9.122, -41.018] => 'Ocean', [18.577, 50.163, -55.230] => 'Eggplant', [26.619, 50.976, 0.503] => 'Maroon', [53.233, 80.109, 67.220] => 'Maraschino', [97.138, -21.556, 94.482] => 'Lemon', [87.737, -86.185, 83.181] => 'Spring', [91.117, -48.080, -14.138] => 'Turquoise', [32.303, 79.197, -107.864] => 'Blueberry', [60.320, 98.254, -60.843] => 'Magenta', [67.050, 42.832, 74.026] => 'Tangerine', [89.910, -67.789, 85.826] => 'Lime', [88.485, -76.749, 46.572] => 'Sea Foam', [54.719, 18.790, -70.925] => 'Aqua', [40.911, 83.182, -93.300] => 'Grape', [54.885, 84.552, 4.066] => 'Strawberry', [63.112, 58.245, 30.548] => 'Salmon', [97.527, -18.441, 70.899] => 'Banana', [89.535, -69.168, 59.807] => 'Flora', [92.382, -39.947, -12.114] => 'Ice', [51.322, 43.621, -76.298] => 'Orchid', [68.008, 76.237, -48.630] => 'Bubblegum', [8.757, 0.001, -0.002] => 'Lead', [91.293, 0.005, -0.010] => 'Mercury', [84.692, 7.020, 56.682] => 'Cantaloupe', [94.019, -38.132, 66.065] => 'Honeydew', [91.100, -52.338, 12.396] => 'Spindrift', [78.011, -15.169, -33.928] => 'Sky', [61.273, 64.277, -59.740] => 'Lavender', [67.377, 65.168, -23.019] => "Carnation", [0.000, 0.000, 0.000] => "Licorice", [100.000, 0.005, -0.010] => 'Snow' }
+end
+
+# EasyRGB - RGB to XYZ
+def rgb_to_xyz(r,g,b)
+  r = r * 1.0
+  g = g * 1.0
+  b = b * 1.0
+
+  var_R = ( r / 255.0 )        #R from 0 to 255
+  var_G = ( g / 255.0 )        #G from 0 to 255
+  var_B = ( b / 255.0 )        #B from 0 to 255
+
+  if ( var_R > 0.04045 ) 
+    var_R = ( ( var_R + 0.055 ) / 1.055 ) ** 2.4
+  else                   
+    var_R = var_R / 12.92
+  end
+  
+  if ( var_G > 0.04045 ) 
+    var_G = ( ( var_G + 0.055 ) / 1.055 ) ** 2.4
+  else
+    var_G = var_G / 12.92
+  end
+
+  if ( var_B > 0.04045 )
+    var_B = ( ( var_B + 0.055 ) / 1.055 ) ** 2.4
+  else
+    var_B = var_B / 12.92
+  end
+
+  var_R = var_R * 100
+  var_G = var_G * 100
+  var_B = var_B * 100
+
+  # Observer. = 2°, Illuminant = D65
+  x = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805
+  y = var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722
+  z = var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505
+  
+  return [x,y,z]
+end
+
+# EasyRGB - XYZ to CIELAB
+def xyz_to_lab(x,y,z)
+  var_X = x / 95.047            #ref_X =  95.047   Observer= 2°, Illuminant= D65
+  var_Y = y / 100.000           #ref_Y = 100.000
+  var_Z = z / 108.883           #ref_Z = 108.883
+
+  if ( var_X > 0.008856 ) 
+    var_X = var_X ** ( 1.0/3.0 )
+  else
+    var_X = ( 7.787 * var_X ) + ( 16.0 / 116.0 )
+  end
+  
+  if ( var_Y > 0.008856 ) 
+    var_Y = var_Y ** ( 1.0/3.0 )
+  else
+    var_Y = ( 7.787 * var_Y ) + ( 16.0 / 116.0 )
+  end
+  
+  if ( var_Z > 0.008856 )
+    var_Z = var_Z ** ( 1.0/3.0 )
+  else
+    var_Z = ( 7.787 * var_Z ) + ( 16.0 / 116.0 )
+  end
+
+  l = ( 116.0 * var_Y ) - 16.0
+  a = 500.0 * ( var_X - var_Y )
+  b = 200.0 * ( var_Y - var_Z )
+
+  return [l.round(3),a.round(3),b.round(3)]
+end
+
+# EasyRGB - RGB to LAB
+# - LAB gives us a perceptually accurate colorspace
+def rgb_to_lab(r,g,b)
+  x,y,z = rgb_to_xyz(r,g,b)
+  l,a,b = xyz_to_lab(x,y,z)
 end
 
 # Euclidean distance
@@ -121,16 +208,27 @@ def save_details(identifier)
     )
   
   # ChunkyPNG for cropping
-  img = ChunkyPNG::Image.from_file("#{identifier}_thumb.png")
-  point_of_interest = center_point(img)
-  img.crop_square_at_center_point(point_of_interest[0], point_of_interest[1], 90).save("#{identifier}_cropped.png")
+  begin
+    img = ChunkyPNG::Image.from_file("#{identifier}_thumb.png")
+    point_of_interest = center_point(img)
+    img.crop_square_at_center_point(point_of_interest[0], point_of_interest[1], 90).save("#{identifier}_cropped.png")
+  rescue
+    return nil
+  end
 
   # Detect dominant colors
+  # - Returns sorted array of pixel RGB colors, converted to LAB
   colors = Miro::DominantColors.new("#{identifier}_cropped.png")
+  colors_lab = colors.to_rgb.collect{|r,g,b| rgb_to_lab(r,g,b)}
+  puts colors_lab.size
   
-  # Map each dominant color to img_key_colors
-  img_key_colors = colors.to_rgb.collect{|color| key_colors[key_colors.keys.sort_by {|c| dist(color, c) }.first]}
-  
+  # Kmeans cluster
+  data = Ai4r::Data::DataSet.new(:data_items => colors_lab)
+  kmeans = Ai4r::Clusterers::KMeans.new
+  result = kmeans.build(data, 10)
+
+  # Map each color cluster to img_key_colors
+  img_key_colors = result.centroids.collect{|color| key_colors[key_colors.keys.sort_by {|c| dist(color, c) }.first]}
   puts "Colors: #{img_key_colors.uniq.inspect}"
 
   # Build 
