618ac1e6f46f44b2135c171d98c5e66c21ded46c
Eric Larson <ewlarson@gmail.com>
2013-02-21 12:40:27 -0600

    Remove duplicate code passages from LAB conversion

--- lib/kolors/lab.rb
+++ lib/kolors/lab.rb
@@ -18,28 +18,22 @@ module Kolors
     
     private
 
+    def xyz_standardize(var)
+      if ( var**3 > 0.008856 ) 
+        var = var**3
+      else                      
+        var = ( var - 16 / 116 ) / 7.787
+      end
+    end
+
     def lab_to_xyz(l,a,b)
       var_Y = ( l + 16 ) / 116
       var_X = a / 500 + var_Y
       var_Z = var_Y - b / 200
 
-      if ( var_Y**3 > 0.008856 ) 
-        var_Y = var_Y**3
-      else                      
-        var_Y = ( var_Y - 16 / 116 ) / 7.787
-      end
-  
-      if ( var_X**3 > 0.008856 ) 
-        var_X = var_X**3
-      else                      
-        var_X = ( var_X - 16 / 116 ) / 7.787
-      end
-  
-      if ( var_Z**3 > 0.008856 ) 
-        var_Z = var_Z**3
-      else
-        var_Z = ( var_Z - 16 / 116 ) / 7.787
-      end
+      var_Y = xyz_standardize(var_Y)
+      var_X = xyz_standardize(var_X)
+      var_Z = xyz_standardize(var_Z)
 
       x = 95.047 * var_X      # ref_X =  95.047     Observer= 2°, Illuminant= D65
       y = 100.000 * var_Y     # ref_Y = 100.000
@@ -48,6 +42,15 @@ module Kolors
       return [x,y,z]
     end
 
+    def rgb_standardize(var)
+      if ( var > 0.0031308 ) 
+        var = 1.055 * ( var ** ( 1 / 2.4 ) ) - 0.055
+      else
+        var = 12.92 * var
+      end
+      var * 255
+    end
+    
     def xyz_to_rgb(x,y,z)
       var_X = x / 100        # X from 0 to  95.047      (Observer = 2°, Illuminant = D65)
       var_Y = y / 100        # Y from 0 to 100.000
@@ -56,28 +59,10 @@ module Kolors
       var_R = var_X *  3.2406 + var_Y * -1.5372 + var_Z * -0.4986
       var_G = var_X * -0.9689 + var_Y *  1.8758 + var_Z *  0.0415
       var_B = var_X *  0.0557 + var_Y * -0.2040 + var_Z *  1.0570
-
-      if ( var_R > 0.0031308 ) 
-        var_R = 1.055 * ( var_R ** ( 1 / 2.4 ) ) - 0.055
-      else
-        var_R = 12.92 * var_R
-      end
-  
-      if ( var_G > 0.0031308 ) 
-        var_G = 1.055 * ( var_G ** ( 1 / 2.4 ) ) - 0.055
-      else
-        var_G = 12.92 * var_G
-      end
-  
-      if ( var_B > 0.0031308 ) 
-        var_B = 1.055 * ( var_B ** ( 1 / 2.4 ) ) - 0.055
-      else
-        var_B = 12.92 * var_B
-      end
-  
-      r = var_R * 255
-      g = var_G * 255
-      b = var_B * 255
+      
+      r = rgb_standardize(var_R)
+      g = rgb_standardize(var_G)
+      b = rgb_standardize(var_B)
   
       return [r,g,b]
     end
