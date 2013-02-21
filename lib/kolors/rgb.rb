488b88e68a3bf53edce2503b49410d111ea2f6b2
Eric Larson <ewlarson@gmail.com>
2013-02-21 13:08:44 -0600

    Remove duplicate code passages from RGB conversion

--- lib/kolors/rgb.rb
+++ lib/kolors/rgb.rb
@@ -17,6 +17,15 @@ module Kolors
     end
   
     protected
+    
+    def rgb_standardize(var)
+      if ( var > 0.04045 )
+        var = ( ( var + 0.055 ) / 1.055 ) ** 2.4
+      else
+        var = var / 12.92
+      end
+      var * 100
+    end
   
     # EasyRGB - RGB to XYZ
     def rgb_to_xyz(r,g,b)
@@ -27,28 +36,10 @@ module Kolors
       var_R = ( r / 255.0 )        #R from 0 to 255
       var_G = ( g / 255.0 )        #G from 0 to 255
       var_B = ( b / 255.0 )        #B from 0 to 255
-
-      if ( var_R > 0.04045 ) 
-        var_R = ( ( var_R + 0.055 ) / 1.055 ) ** 2.4
-      else                   
-        var_R = var_R / 12.92
-      end
-  
-      if ( var_G > 0.04045 ) 
-        var_G = ( ( var_G + 0.055 ) / 1.055 ) ** 2.4
-      else
-        var_G = var_G / 12.92
-      end
-
-      if ( var_B > 0.04045 )
-        var_B = ( ( var_B + 0.055 ) / 1.055 ) ** 2.4
-      else
-        var_B = var_B / 12.92
-      end
-
-      var_R = var_R * 100
-      var_G = var_G * 100
-      var_B = var_B * 100
+      
+      var_R = rgb_standardize(var_R)
+      var_G = rgb_standardize(var_G)
+      var_B = rgb_standardize(var_B)
 
       # Observer. = 2°, Illuminant = D65
       x = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805
@@ -57,30 +48,24 @@ module Kolors
   
       return [x,y,z]
     end
-
+    
+    def xyz_standardize(var)
+      if ( var > 0.008856 ) 
+        var = var ** ( 1.0/3.0 )
+      else
+        var = ( 7.787 * var ) + ( 16.0 / 116.0 )
+      end
+    end
+    
     # EasyRGB - XYZ to CIELAB
     def xyz_to_lab(x,y,z)
       var_X = x / 95.047            # ref_X =  95.047   Observer= 2°, Illuminant= D65
       var_Y = y / 100.000           # ref_Y = 100.000
       var_Z = z / 108.883           # ref_Z = 108.883
-
-      if ( var_X > 0.008856 ) 
-        var_X = var_X ** ( 1.0/3.0 )
-      else
-        var_X = ( 7.787 * var_X ) + ( 16.0 / 116.0 )
-      end
-  
-      if ( var_Y > 0.008856 ) 
-        var_Y = var_Y ** ( 1.0/3.0 )
-      else
-        var_Y = ( 7.787 * var_Y ) + ( 16.0 / 116.0 )
-      end
-  
-      if ( var_Z > 0.008856 )
-        var_Z = var_Z ** ( 1.0/3.0 )
-      else
-        var_Z = ( 7.787 * var_Z ) + ( 16.0 / 116.0 )
-      end
+      
+      var_X = xyz_standardize(var_X)
+      var_Y = xyz_standardize(var_Y)
+      var_Z = xyz_standardize(var_Z)
 
       l = ( 116.0 * var_Y ) - 16.0
       a = 500.0 * ( var_X - var_Y )
