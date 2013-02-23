e4840e9979a51c7a8d75dbae20c53f116368e8d7
Eric Larson <ewlarson@gmail.com>
2013-02-23 14:45:50 -0600

    Add a few basic dominant color specs

--- spec/lib/kolors/dominant_colors_spec.rb
+++ spec/lib/kolors/dominant_colors_spec.rb
@@ -2,10 +2,29 @@ require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 
 describe Kolors::DominantColors do
   before(:each) do
-    @kolors = Kolors::DominantColors.new('../../fixtures/red_tetrad.png')
+    @kolors = Kolors::DominantColors.new(file_path('red_tetrad.png'))
+    @red_tetrad = [{"Teal"=>25.0}, {"Tangerine"=>25.0}, {"Maraschino"=>25.0}, {"Clover"=>25.0}] 
   end
   
-  it "should properly cluster a red tetrad" do
+  # KMeans clustering
+  it "should properly cluster a red tetrad to LAB colorspace" do
     @kolors.to_lab.should match_array([[50.861, -24.406, -8.025], [59.711, 48.424, 61.115], [53.233, 80.109, 67.22], [66.882, -55.641, 61.082]])
   end
+  
+  # KMeans-based facet colors
+  it "should build the correct facets" do
+    # Color => Percentage
+    @kolors.to_facets.each do |facet|
+      @red_tetrad.should include(facet)
+    end
+  end
+  
+  # Pixel-count-based facet colors
+  it "should properly color bin the pixel counts" do
+    # Color => Pixel Count Percentage
+    @kolors.color_bins_result.each do |facet|
+      @red_tetrad.should include(facet)
+    end
+  end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ require 'bundler/setup'
 
 require 'kolors' # and any other gems you need
 
+def file_path( *paths )
+  File.expand_path(File.join(File.dirname(__FILE__), 'fixtures', *paths))
+end
+
 RSpec.configure do |config|
   # some (optional) config here
 end
